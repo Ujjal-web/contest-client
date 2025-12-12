b670c604dca5bf86be668d40d9d205a9e5f11302
ujjal-web <ujjaldas827@gmail.com>
2025-12-12 13:50:51 +0600

    firebase sdk added

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA70inkItgaRCqCR5-K2ON0SIKySnouqxw",
+    authDomain: "contest-hub-a11.firebaseapp.com",
+    projectId: "contest-hub-a11",
+    storageBucket: "contest-hub-a11.firebasestorage.app",
+    messagingSenderId: "900866099136",
+    appId: "1:900866099136:web:181d1f2df559087925aa25"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
