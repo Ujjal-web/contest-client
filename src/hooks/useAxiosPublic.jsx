6286953062130167bc19df72d774a723f463cb0c
Ujjal-web <ujjaldas827@gmail.comm>
2025-12-22 02:04:44 +0600

    updated for deployment

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000' //Backend URL
+    baseURL: import.meta.env.VITE_API_URL || 'https://contest-server-11.vercel.app' //Backend URL
 });
+
 
 const useAxiosPublic = () => {
     return axiosPublic;
